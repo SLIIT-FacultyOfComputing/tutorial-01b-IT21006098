5c05cc41236c054492d26e3f2565138910bbfac7
IT21006098 <87415366+IT21006098@users.noreply.github.com>
2021-07-14 14:01:11 +0000

    n

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,26 @@
 #include <stdio.h>
 
 int main() {
+
+	int subject1 ,subject2,sum;
+	float avg;
+	
+	
+	
+    printf("enter the subject1 :");
+    scanf("%d",&subject1);
+    printf("enter the subject1 :");
+    scanf("%d",&subject2);
+    
+  sum  =  subject1 + subject2;
   
-  return 0;
+  avg  = (float)sum /2;
+    
+    printf(" avg mark is :%.2f\n",avg);
+    
+    
+	return 0;
 }
+
+
 
